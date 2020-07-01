98c2e3edc0f53c5aa8a7fbc38c077045c68a1a35
wolvecode <wolvecode@gmail.com>
2020-07-01 23:54:30 +0100

    changes

--- src/components/views/NavBar/Sections/RightMenu.js
+++ src/components/views/NavBar/Sections/RightMenu.js
@@ -8,52 +8,55 @@ import { useSelector } from 'react-redux'
 // import { AiOutlineShoppingCart } from 'react-icons/ai'
 
 function RightMenu(props) {
-  const user = useSelector((state) => state.user)
+   const user = useSelector((state) => state.user)
 
-  const logoutHandler = () => {
-    axios.get(`${USER_SERVER}/logout`).then((response) => {
-      if (response.status === 200) {
-        props.history.push('/login')
-      } else {
-        alert('Log Out Failed')
-      }
-    })
-  }
+   const logoutHandler = () => {
+      axios.get(`${USER_SERVER}/logout`).then((response) => {
+         if (response.status === 200) {
+            props.history.push('/login')
+         } else {
+            alert('Log Out Failed')
+         }
+      })
+   }
 
-  if (user.userData && !user.userData.isAuth) {
-    return (
-      <Menu mode={props.mode}>
-        <Menu.Item key='mail'>
-          <a href='/login'>Signin</a>
-        </Menu.Item>
-        <Menu.Item key='app'>
-          <a href='/register'>Signup</a>
-        </Menu.Item>
-      </Menu>
-    )
-  } else {
-    return (
-      <Menu mode={props.mode}>
-        <Menu.Item key='cart'>
-          <Badge count={1}>
-            <a href='/user/cart' style={{ marginRight: -15, color: '#667777' }}>
-              <Icon
-                type='shopping-cart'
-                style={{ fontSize: 30, marginBottom: 4 }}
-              />
-            </a>
-          </Badge>
-        </Menu.Item>
-        <Menu.Item key='upload'>
-          <a href='/product/upload'>Upload</a>
-        </Menu.Item>
+   if (user.userData && !user.userData.isAuth) {
+      return (
+         <Menu mode={props.mode}>
+            <Menu.Item key='mail'>
+               <a href='/login'>Signin</a>
+            </Menu.Item>
+            <Menu.Item key='app'>
+               <a href='/register'>Signup</a>
+            </Menu.Item>
+         </Menu>
+      )
+   } else {
+      return (
+         <Menu mode={props.mode}>
+            <Menu.Item key='cart'>
+               <Badge count={1}>
+                  <a
+                     href='/user/cart'
+                     style={{ marginRight: -15, color: '#667777' }}
+                  >
+                     <Icon
+                        type='shopping-cart'
+                        style={{ fontSize: 30, marginBottom: 4 }}
+                     />
+                  </a>
+               </Badge>
+            </Menu.Item>
+            <Menu.Item key='upload'>
+               <a href='/product/upload'>Upload</a>
+            </Menu.Item>
 
-        <Menu.Item key='logout'>
-          <a onClick={logoutHandler}>Logout</a>
-        </Menu.Item>
-      </Menu>
-    )
-  }
+            <Menu.Item key='logout'>
+               <a onClick={logoutHandler}>Logout</a>
+            </Menu.Item>
+         </Menu>
+      )
+   }
 }
 
 export default withRouter(RightMenu)
