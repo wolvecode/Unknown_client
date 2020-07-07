747708ba1e7241574a6da82e9f2c885093347dac
wolvecode <wolvecode@gmail.com>
2020-07-07 23:22:08 +0100

    changes

--- src/components/Utils/RavePayment.js
+++ src/components/Utils/RavePayment.js
@@ -1,34 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import { useRavePayment } from 'react-ravepayment'
+import React, { Component } from 'react'
+import Rave from 'react-flutterwave-rave'
 
-const RavePayment = (props) => {
-   const config = {
-      txref: 'rave-123456',
-      customer_email: 'wolvecode@gmail.com',
-      customer_phone: '234099940409',
-      amount: props.toPay,
-      PBFPubKey: 'FLWPUBK_TEST-1698436ccb9a38d881340a6c01559c12-X',
-      production: false,
+class RavePayment extends Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         key: 'FLWPUBK_TEST-1698436ccb9a38d881340a6c01559c12-X', // RavePay PUBLIC KEY
+         phone: '0000000000000',
+         amount: this.props.toPay,
+         firstname: 'Oluwole',
+         lastname: 'Adebiyi',
+         email: this.props.user,
+      }
+      this.onSuccess = this.onSuccess.bind(this)
+      this.onCancel = this.onCancel.bind(this)
+   }
+   componentDidMount() {
+      console.log(this.props.info)
+   }
+   onSuccess = (response) => {
+      console.log('The paymesnt was succesful', response)
+      this.props.onSuccess(response)
    }
 
-   const { initializePayment } = useRavePayment(config)
-   return (
-      <div>
-         <button
+   onCancel = () => {
+      console.log('Payment cancel')
+   }
+
+   render() {
+      return (
+         <div
             style={{
-               width: '20rem',
-               height: '3rem',
-               color: '#fff',
-               fontSize: '1.2rem',
-               background: '#2980b9',
-               border: 'none',
+               width: '200rem',
+               fontSize: '1rem',
+               fontWeight: 'bold',
             }}
-            onClick={() => initializePayment()}
          >
-            Make Payment
-         </button>
-      </div>
-   )
+            <Rave
+               pay_button_text='Pay With Rave'
+               class='button'
+               redirect_url='http://localhost:3000/user/cart'
+               payment_method='card'
+               customer_firstname={this.state.firstname}
+               customer_lastname={this.state.lastname}
+               customer_email={this.state.email}
+               customer_phone={this.state.phone}
+               amount={this.state.amount}
+               ravePubKey={this.state.key}
+               callback={this.onSuccess}
+               onclose={this.onCancel}
+            />
+         </div>
+      )
+   }
 }
 
 export default RavePayment
