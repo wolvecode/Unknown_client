6a2693e3ab79567b613f6455199143b3196b5fd6
wolvecode <wolvecode@gmail.com>
2020-07-12 21:27:14 +0100

    changes and make changes

--- src/components/views/HistoryPage/HistoryPage.js
+++ src/components/views/HistoryPage/HistoryPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
+import { Collapse } from 'antd'
 
 function HistoryPage() {
    const [History, setHistory] = useState([])
@@ -13,8 +14,36 @@ function HistoryPage() {
       })
    }, [History])
    return (
-      <div>
-         <h1>Hello world</h1>
+      <div style={{ margin: '3rem', width: '80%' }}>
+         <div style={{ textAlign: 'center', fontWeight: 'bolder' }}>
+            <h1>History</h1>
+         </div>
+         <div>
+            <table
+               style={{
+                  borderCollapse: 'collapse',
+                  width: '100%',
+                  a: 'center',
+               }}
+            >
+               <tr>
+                  <th>Payemend Id</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Date Of Purchase</th>
+               </tr>
+               {History.map((item) => (
+                  <tr key={item._id}>
+                     <td style={{ border: '1px', padding: '0.5rem' }}>
+                        {item.id}
+                     </td>
+                     <td>{item.price}</td>
+                     <td>{item.quantity}</td>
+                     <td>{item.dateOfPurchase}</td>
+                  </tr>
+               ))}
+            </table>
+         </div>
       </div>
    )
 }
