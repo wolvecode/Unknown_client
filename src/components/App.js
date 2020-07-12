76327bfc892557a5f4686a60d652288b031bb611
wolvecode <wolvecode@gmail.com>
2020-07-12 21:26:34 +0100

    add history page to route

--- src/components/App.js
+++ src/components/App.js
@@ -10,36 +10,46 @@ import Footer from './views/Footer/Footer'
 import UploadProduct from './views/Upload/UploadProduct'
 import DetailPage from './views/DetailFood/DetailFood'
 import Cart from './views/Cart/Cart'
+import HistoryPage from './views/HistoryPage/HistoryPage'
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
 
 function App() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
-        <Switch>
-          <Route exact path='/' component={Auth(LandingPage, null)} />
-          <Route exact path='/login' component={Auth(LoginPage, false)} />
-          <Route exact path='/register' component={Auth(RegisterPage, false)} />
-          <Route exact path='/user/cart' component={Auth(Cart, true)} />
-          <Route
-            exact
-            path='/product/upload'
-            component={Auth(UploadProduct, true)}
-          />
-          <Route
-            exact
-            path='/product/:foodId'
-            component={Auth(DetailPage, null)}
-          />
-        </Switch>
-      </div>
-      <Footer />
-    </Suspense>
-  )
+   return (
+      <Suspense fallback={<div>Loading...</div>}>
+         <NavBar />
+         <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+            <Switch>
+               <Route exact path='/' component={Auth(LandingPage, null)} />
+               <Route exact path='/login' component={Auth(LoginPage, false)} />
+               <Route
+                  exact
+                  path='/register'
+                  component={Auth(RegisterPage, false)}
+               />
+               <Route exact path='/user/cart' component={Auth(Cart, true)} />
+               <Route
+                  exact
+                  path='/history'
+                  component={Auth(HistoryPage, true)}
+               />
+               <Route
+                  exact
+                  path='/product/upload'
+                  component={Auth(UploadProduct, true)}
+               />
+               <Route
+                  exact
+                  path='/product/:foodId'
+                  component={Auth(DetailPage, null)}
+               />
+            </Switch>
+         </div>
+         <Footer />
+      </Suspense>
+   )
 }
 
 export default App
