709ae76d79fa1ff4e9bade5728a6c534a89d35a5
wolvecode <wolvecode@gmail.com>
2020-07-14 23:51:52 +0100

    remove home and blog

--- src/components/views/NavBar/Sections/LeftMenu.js
+++ src/components/views/NavBar/Sections/LeftMenu.js
@@ -6,10 +6,10 @@ const MenuItemGroup = Menu.ItemGroup
 function LeftMenu(props) {
    return (
       <Menu mode={props.mode}>
-         <Menu.Item key='mail'>
+         {/* <Menu.Item key='mail'>
             <a href='/'>Home</a>
-         </Menu.Item>
-         <SubMenu title={<span>Blogs</span>}>
+         </Menu.Item> */}
+         {/* <SubMenu title={<span>Blogs</span>}>
             <MenuItemGroup title='Item 1'>
                <Menu.Item key='setting:1'>Option 1</Menu.Item>
                <Menu.Item key='setting:2'>Option 2</Menu.Item>
@@ -18,7 +18,7 @@ function LeftMenu(props) {
                <Menu.Item key='setting:3'>Option 3</Menu.Item>
                <Menu.Item key='setting:4'>Option 4</Menu.Item>
             </MenuItemGroup>
-         </SubMenu>
+         </SubMenu> */}
       </Menu>
    )
 }
