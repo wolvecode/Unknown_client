9849c61d98d04ec83bb0fe0d243bacf0b5a567e9
wolvecode <wolvecode@gmail.com>
2020-07-20 12:47:24 +0100

    add section for desktop view

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -1,20 +1,71 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <div
-      style={{
-        height: '80px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize: '1rem',
-      }}
-    >
-      <p> The Footer Goest Here </p>
-    </div>
-  )
+   return (
+      <div className='footer'>
+         <div className='section'>
+            <div className='footer-section'>
+               <div className='footer-column'>
+                  <h3>For Company</h3>
+                  <a href=''>
+                     <p>Aboutus</p>
+                  </a>
+                  <a href=''>
+                     <p>Privacy Poilicy</p>
+                  </a>
+                  <a href=''>
+                     <p>Terms And Condition</p>
+                  </a>
+               </div>
+               <div className='footer-column'>
+                  <h3>For Customer</h3>
+                  <a href=''>
+                     <p>Login</p>
+                  </a>
+                  <a href=''>
+                     <p>Sign Up</p>
+                  </a>
+               </div>
+               <div className='footer-column'>
+                  <h3>For Restaurant</h3>
+                  <a href=''>
+                     <p>Restaurant Login</p>
+                  </a>
+                  <a href=''>
+                     <p>Registration</p>
+                  </a>
+               </div>
+               {/* <div className='footer-column'>Hello world</div> */}
+            </div>
+            <div className='footer-line'></div>
+            <div className='footer-copyright'>
+               <div className='left'>
+                  <p>
+                     © Copyrights <span>AT&A Retail Limited.</span> All Rights
+                     Reserved.
+                  </p>
+               </div>
+               <div className='right'>
+                  <a href=''>
+                     <i class='fab fa-twitter-square'></i>
+                  </a>
+                  <a href=''>
+                     <i class='fab fa-instagram-square'></i>
+                  </a>
+                  <a href=''>
+                     <i class='fab fa-linkedin'></i>
+                  </a>
+                  <a href=''>
+                     <i class='fab fa-whatsapp-square'></i>
+                  </a>
+                  <a href=''>
+                     <i class='fab fa-linkedin'></i>
+                  </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
 }
 
 export default Footer
