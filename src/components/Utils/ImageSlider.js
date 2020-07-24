147f86205e557a227801c6681a858b0c4b5163c7
wolvecode <wolvecode@gmail.com>
2020-07-24 22:13:39 +0100

    format

--- src/components/Utils/ImageSlider.js
+++ src/components/Utils/ImageSlider.js
@@ -2,21 +2,21 @@ import React from 'react'
 import { Carousel } from 'antd'
 
 function ImageSlider(props) {
-  return (
-    <div>
-      <Carousel autoplay>
-        {props.images.map((image, index) => (
-          <div key={index}>
-            <img
-              style={{ width: '100%', height: '120px' }}
-              src={`http://localhost:5000/${image}`}
-              alt='FoodImage'
-            />
-          </div>
-        ))}
-      </Carousel>
-    </div>
-  )
+   return (
+      <div>
+         <Carousel autoplay>
+            {props.images.map((image, index) => (
+               <div key={index}>
+                  <img
+                     style={{ width: '100%', height: '120px' }}
+                     src={`http://localhost:5000/${image}`}
+                     alt='FoodImage'
+                  />
+               </div>
+            ))}
+         </Carousel>
+      </div>
+   )
 }
 
 export default ImageSlider
