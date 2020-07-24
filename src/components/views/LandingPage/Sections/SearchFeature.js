309d743d6e7e8956ae9ccf1f789d5534bafef3e7
wolvecode <wolvecode@gmail.com>
2020-07-24 22:15:29 +0100

    changes

--- src/components/views/LandingPage/Sections/SearchFeature.js
+++ src/components/views/LandingPage/Sections/SearchFeature.js
@@ -4,23 +4,23 @@ import { changeConfirmLocale } from 'antd/lib/modal/locale'
 
 const { Search } = Input
 function SearchFeature(props) {
-  const [SearchTerms, setSearchTerms] = useState('')
-  const changeSearch = (e) => {
-    setSearchTerms(e.target.value)
-    props.refreshFunction(e.target.value)
-  }
-  return (
-    <div>
-      <div style={{ display: 'flex', float: 'right' }}>
-        <Search
-          onChange={changeSearch}
-          value={SearchTerms}
-          placeholder='Search by typing...'
-        />
-        <br />
+   const [SearchTerms, setSearchTerms] = useState('')
+   const changeSearch = (e) => {
+      setSearchTerms(e.target.value)
+      props.refreshFunction(e.target.value)
+   }
+   return (
+      <div>
+         <div style={{ display: 'flex', float: 'right' }}>
+            <Search
+               onChange={changeSearch}
+               value={SearchTerms}
+               placeholder='Search Food by typing...'
+            />
+            <br />
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 
 export default SearchFeature
