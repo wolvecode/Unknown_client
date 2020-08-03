b46b7c8908443b7bd523585eaa11db88210ca9df
wolvecode <wolvecode@gmail.com>
2020-08-03 23:51:18 +0100

    change

--- src/components/views/LandingPage/Sections/RadioBox.js
+++ src/components/views/LandingPage/Sections/RadioBox.js
@@ -4,31 +4,31 @@ import { Radio, Collapse } from 'antd'
 const Panel = Collapse
 
 function RadioBox(props) {
-  const [Value, setValue] = useState('0')
-  const renderRadioList = () =>
-    props.prices.map((price) => (
-      <Radio key={price._id} value={`${price._id}`}>
-        {price.name}
-      </Radio>
-    ))
+   const [Value, setValue] = useState('0')
+   const renderRadioList = () =>
+      props.prices.map((price) => (
+         <Radio key={price._id} value={`${price._id}`}>
+            {price.name}
+         </Radio>
+      ))
 
-  const handleRadio = (e) => {
-    setValue(e.target.value)
-    props.handleFilters(e.target.value)
-  }
-  return (
-    <div>
+   const handleRadio = (e) => {
+      setValue(e.target.value)
+      props.handleFilters(e.target.value)
+   }
+   return (
       <div>
-        <Collapse defaultActiveKey={['0']}>
-          <Panel header='price' key='1'>
-            <Radio.Group onChange={handleRadio} value={Value}>
-              {renderRadioList()}
-            </Radio.Group>
-          </Panel>
-        </Collapse>
+         <div>
+            <Collapse defaultActiveKey={['0']}>
+               <Panel header='price' key='1'>
+                  <Radio.Group onChange={handleRadio} value={Value}>
+                     {renderRadioList()}
+                  </Radio.Group>
+               </Panel>
+            </Collapse>
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 
 export default RadioBox
