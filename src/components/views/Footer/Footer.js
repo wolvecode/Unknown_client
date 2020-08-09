25db9ca299f24ddf7b61479feab98de1f0f8dbd3
wolvecode <wolvecode@gmail.com>
2020-08-09 23:29:18 +0100

    changes

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -6,36 +6,41 @@ function Footer() {
          <div className='section'>
             <div className='footer-section'>
                <div className='footer-column'>
-                  <h3>For Company</h3>
-                  <a href=''>
-                     <p>Aboutus</p>
-                  </a>
-                  <a href=''>
-                     <p>Privacy Poilicy</p>
-                  </a>
-                  <a href=''>
-                     <p>Terms And Condition</p>
-                  </a>
+                  <div className='column1'>
+                     <h3>For Company</h3>
+                     <a href=''>
+                        <p>Aboutus</p>
+                     </a>
+                     <a href=''>
+                        <p>Privacy Poilicy</p>
+                     </a>
+                     <a href=''>
+                        <p>Terms And Condition</p>
+                     </a>
+                  </div>
                </div>
                <div className='footer-column'>
-                  <h3>For Customer</h3>
-                  <a href=''>
-                     <p>Login</p>
-                  </a>
-                  <a href=''>
-                     <p>Sign Up</p>
-                  </a>
+                  <div className='column2'>
+                     <h3>For Customer</h3>
+                     <a href=''>
+                        <p>Login</p>
+                     </a>
+                     <a href=''>
+                        <p>Sign Up</p>
+                     </a>
+                  </div>
                </div>
                <div className='footer-column'>
-                  <h3>For Restaurant</h3>
-                  <a href=''>
-                     <p>Restaurant Login</p>
-                  </a>
-                  <a href=''>
-                     <p>Registration</p>
-                  </a>
+                  <div className='column3'>
+                     <h3>For Restaurant</h3>
+                     <a href=''>
+                        <p>Restaurant Login</p>
+                     </a>
+                     <a href=''>
+                        <p>Registration</p>
+                     </a>
+                  </div>
                </div>
-               {/* <div className='footer-column'>Hello world</div> */}
             </div>
             <div className='footer-line'></div>
             <div className='footer-copyright'>
