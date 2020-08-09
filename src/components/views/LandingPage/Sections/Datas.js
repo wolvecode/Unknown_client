697e67f3f2ad5105fb23597c3e041a00903c95fc
wolvecode <wolvecode@gmail.com>
2020-08-09 23:30:12 +0100

    change in data for shop

--- src/components/views/LandingPage/Sections/Datas.js
+++ src/components/views/LandingPage/Sections/Datas.js
@@ -1,18 +1,18 @@
 const prices = [
-  { _id: 0, name: 'any', array: [] },
-  { _id: 1, name: '$0, $1000', array: [0, 1000] },
-  { _id: 2, name: '$1000 to $1500', array: [1000, 1500] },
-  { _id: 3, name: '$1500 to $2000', array: [1500, 2000] },
-  { _id: 4, name: '$2000 to $2500', array: [2000, 2500] },
-  { _id: 5, name: '$2500 to $3000', array: [2500, 3000] },
+   { _id: 0, name: 'any', array: [] },
+   { _id: 1, name: '$0, $1000', array: [0, 1000] },
+   { _id: 2, name: '$1000 to $1500', array: [1000, 1500] },
+   { _id: 3, name: '$1500 to $2000', array: [1500, 2000] },
+   { _id: 4, name: '$2000 to $2500', array: [2000, 2500] },
+   { _id: 5, name: '$2500 to $3000', array: [2500, 3000] },
 ]
 
 const shops = [
-  { _id: 1, name: 'shop1' },
-  { _id: 2, name: 'shop2' },
-  { _id: 3, name: 'shop3' },
-  { _id: 4, name: 'shop4' },
-  { _id: 5, name: 'shop5' },
+   { _id: 1, name: 'shop1' },
+   { _id: 2, name: 'shop2' },
+   { _id: 3, name: 'shop3' },
+   { _id: 4, name: 'shop4' },
+   { _id: 5, name: 'shop5' },
 ]
 
 export { prices, shops }
