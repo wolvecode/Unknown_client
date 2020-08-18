d4bbdc83e9696ef1485c6f0b8855f1b0595fffd9
wolvecode <wolvecode@gmail.com>
2020-08-18 23:48:54 +0100

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Auth from '../hoc/auth'
-// pages for this product
+// pages for the food
 import LandingPage from './views/LandingPage/LandingPage.js'
 import LoginPage from './views/LoginPage/LoginPage.js'
 import RegisterPage from './views/RegisterPage/RegisterPage.js'
