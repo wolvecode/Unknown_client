f5a382aec1b29b4ba04c88ff0cd420229850b209
wolvecode <wolvecode@gmail.com>
2021-01-01 23:59:34 +0100

    changes

--- src/components/views/DetailFood/DetailFood.js
+++ src/components/views/DetailFood/DetailFood.js
@@ -7,40 +7,53 @@ import { useDispatch } from 'react-redux'
 import axios from 'axios'
 
 function DetailProduct(props) {
-  const dispatch = useDispatch()
+   const dispatch = useDispatch()
 
-  const [Food, setFood] = useState([])
-  const foodId = props.match.params.foodId
+   const [Food, setFood] = useState([])
+   const foodId = props.match.params.foodId
 
-  useEffect(() => {
-    axios
-      .get(
-        `http://localhost:5000/api/product/foods_by_id?id=${foodId}&type=single`
-      )
-      .then((response) => {
-        setFood(response.data[0])
-      })
-  }, [])
+   useEffect(() => {
+      axios
+         .get(
+            `http://localhost:5000/api/product/foods_by_id?id=${foodId}&type=single`
+         )
+         .then((response) => {
+            setFood(response.data[0])
+         })
+   }, [])
 
-  const cartHandler = (foodId) => {
-    dispatch(addToCart(foodId))
-  }
-  return (
-    <div className='postPage' style={{ width: '100%', padding: '3rem 4rem' }}>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <h1>{Food.name} </h1>
+   const cartHandler = (foodId) => {
+      dispatch(addToCart(foodId))
+   }
+   return (
+      <div
+         className='postPage'
+         style={{
+            fontFamily: 'Amaranth, sans-serif',
+            width: '100%',
+            padding: '3rem 4rem',
+         }}
+      >
+         <div
+            style={{
+               fontFamily: 'Amaranth, sans-serif',
+               display: 'flex',
+               justifyContent: 'center',
+            }}
+         >
+            <h1>{Food.name} </h1>
+         </div>
+         <br />
+         <Row gutter={[16, 16]}>
+            <Col lg={12} xs={24} md={12}>
+               <FoodImage detail={Food} />
+            </Col>
+            <Col lg={12} xs={24} md={12}>
+               <FoodInfo addToCart={cartHandler} detail={Food} />
+            </Col>
+         </Row>
       </div>
-      <br />
-      <Row gutter={[16, 16]}>
-        <Col lg={12} xs={24} md={12}>
-          <FoodImage detail={Food} />
-        </Col>
-        <Col lg={12} xs={24} md={12}>
-          <FoodInfo addToCart={cartHandler} detail={Food} />
-        </Col>
-      </Row>
-    </div>
-  )
+   )
 }
 
 export default DetailProduct
--- src/components/views/DetailFood/Sections/FoodInfo.js
+++ src/components/views/DetailFood/Sections/FoodInfo.js
@@ -2,41 +2,68 @@ import React, { useEffect, useState } from 'react'
 import { Button, Descriptions } from 'antd'
 
 function FoodInfo(props) {
-  const [Food, setFood] = useState([])
+   const [Food, setFood] = useState([])
 
-  useEffect(() => {
-    setFood(props.detail)
-  }, [props.detail])
+   useEffect(() => {
+      setFood(props.detail)
+   }, [props.detail])
 
-  const cartHandler = () => {
-    props.addToCart(props.detail._id)
-  }
-  return (
-    <div>
-      <Descriptions title='Food Info'>
-        <Descriptions.Item label='Price'> {Food.price} </Descriptions.Item>
-        <Descriptions.Item label='Sold'> {Food.sold} </Descriptions.Item>
-        <Descriptions.Item label='View'> {Food.view} </Descriptions.Item>
-        <Descriptions.Item label='Description'>
-          {Food.description}
-        </Descriptions.Item>
-      </Descriptions>
+   const cartHandler = () => {
+      props.addToCart(props.detail._id)
+   }
+   return (
+      <div>
+         <Descriptions title='Food Info'>
+            <Descriptions.Item
+               style={{ fontFamily: 'Amaranth, sans-serif' }}
+               label='Price'
+               size={'middle'}
+            >
+               {' '}
+               {Food.price}{' '}
+            </Descriptions.Item>
+            <Descriptions.Item
+               style={{ fontFamily: 'Amaranth, sans-serif' }}
+               label='Sold'
+            >
+               {' '}
+               {Food.sold}{' '}
+            </Descriptions.Item>
+            <Descriptions.Item
+               style={{ fontFamily: 'Amaranth, sans-serif' }}
+               label='View'
+            >
+               {' '}
+               {Food.view}{' '}
+            </Descriptions.Item>
+            <Descriptions.Item
+               style={{ fontFamily: 'Amaranth, sans-serif' }}
+               label='Description'
+            >
+               {Food.description}
+            </Descriptions.Item>
+         </Descriptions>
 
-      <br />
-      <br />
-      <br />
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <Button onClick={cartHandler} type='danger' shape='round' size='large'>
-          Add To Cart
-        </Button>
+         <br />
+         <br />
+         <br />
+         <div
+            style={{
+               display: 'flex',
+               justifyContent: 'center',
+            }}
+         >
+            <Button
+               onClick={cartHandler}
+               type='danger'
+               shape='round'
+               size='large'
+            >
+               Add To Cart
+            </Button>
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 
 export default FoodInfo
